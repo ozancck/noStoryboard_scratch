c45db2fc0cd54621a7715efc9e5dcafaa49aea55
Ozan Çiçek <134602322+ozancicekisteportal@users.noreply.github.com>
2024-02-25 15:32:31 +0300

    first commit

---
+++ noStoryboard/CreateVC.swift
@@ -0,0 +1,21 @@
+//
+//  CreateVC.swift
+//  noStoryboard
+//
+//  Created by Ozan Çiçek on 24.02.2024.
+//
+
+import UIKit
+
+class CreateVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+        
+    }
+    
+
+
+
+}
---
+++ noStoryboard/SearchVC.swift
@@ -0,0 +1,22 @@
+//
+//  SearchVC.swift
+//  noStoryboard
+//
+//  Created by Ozan Çiçek on 24.02.2024.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        view.backgroundColor = .systemRed
+        
+    }
+    
+
+
+}
